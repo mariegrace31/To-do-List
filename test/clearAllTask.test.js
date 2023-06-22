7cb7a379dd4ee1957bb82e6339587156bbeb02e1
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-06-22 17:02:03 +0300

    Created clearAllTask.test.js file and it passed the test

---
+++ test/clearAllTask.test.js
@@ -0,0 +1,14 @@
+import { clearAllCompletedTasks } from '../src/module/clearTask';
+
+describe('clearAllCompleted', () => {
+  const tasks = [
+    { description: 'Task1', completed: false },
+    { description: 'Task2', completed: true },
+    { description: 'Task3', completed: false },
+  ];
+
+  test('clear all completed tasks from the list', () => {
+    const newArray = tasks.filter((task) => !task.completed);
+    expect(newArray.length).toBe(2);
+  });
+});
